79d57c2e9671cb1a8085eaa06d693a7d6aa5869d
Gregory Mckenzie <gregoryjmckenzie7@gmail.com>
2018-04-23 22:46:41 -0400

    Comment Schema

---
+++ src/models/Comments.js
@@ -0,0 +1,36 @@
+
+const mongoose = require("mongoose");
+
+const commentSchema = new mongoose.Schema({
+    commentId: {
+        type: String,
+        require: true
+    },
+    ratingNumber: {
+        type: Number,
+        require: true
+    },
+    commentedBy: {
+        type: String,
+        require: true
+    },
+    timestamps: {
+        createdAt: 'created_at',
+        require: true
+    },
+    meetingId: {
+        type: String,
+        require: true
+    },
+    comment: {
+        type: String,
+        require: true
+    },
+    tagArray: {
+        type: Array,
+        require: false
+    }
+});
+
+
+module.exports = mongoose.model("Comments", commentSchema);
