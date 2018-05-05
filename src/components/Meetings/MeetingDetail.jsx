22157d8dd9d85bc771fd367ecf9d1dd8bd9e84d1
rocsteady888 <rocsteady888@gmail.com>
2018-05-04 23:55:36 -0400

    fixed issue with setState method on line 29

--- src/components/Meetings/MeetingDetail.jsx
+++ src/components/Meetings/MeetingDetail.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Bar } from 'react-chartjs-2';
+import { Bar, Bubble } from 'react-chartjs-2';
 
 class MeetingDetail extends React.Component {
   constructor(props) {
@@ -9,6 +9,9 @@ class MeetingDetail extends React.Component {
       meetingId: "",
       ended: "",
       resLabel: [],
+      resTimestamp: [],
+      resCommentAbout: [],
+      resCommentBy: [],
       resData: [],
       charData: {}
     };
@@ -23,25 +26,27 @@ class MeetingDetail extends React.Component {
       .then(res => {
         const comments = res.data.comments;
         comments.map(comment => {
-          this.setState({ resData: [...this.state.resData, comment.rating] })
+          this.setState({ 
+            resData: [...this.state.resData, comment.rating],
+            resCommentAbout: [...this.state.resLabel, comment.commentAbout],
+            resCommentBy: [...this.state.resLabel, comment.commentBy],
+            resTimestamp: [...this.state.resLabel, comment.timestamp],
+            resLabel: [...this.state.resLabel, comment.commentAbout],
+            charData: {
+              labels: this.state.resLabel,
+              datasets: [{
+                label: "Dataset #1",
+                backgroundColor: "rgba(255,99,132,0.2)",
+                borderColor: "rgba(255,99,132,1)",
+                borderWidth: 2,
+                hoverBackgroundColor: "rgba(255,99,132,0.4)",
+                hoverBorderColor: "rgba(255,99,132,1)",
+                data: this.state.resData
+              }]
+            }
+          })
         })
-        comments.map(comment => {
-          this.setState({ resLabel: [...this.state.resLabel, comment.commentAbout] });
-        })
-        this.setState({charData:{
-          labels: this.state.resLabel,
-          datasets: [{
-            label: "Dataset #1",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 2,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: this.state.resData
-          }]
-        }})
       })
-      // .then(() => this.state.resData.shift())
       .catch(err => console.log(err));
     // this.setState({ mode });
     }
@@ -72,6 +77,19 @@ class MeetingDetail extends React.Component {
                   }
                   }}
               />
+                <Bubble
+                  data={this.state.charData}
+                  //dynamically rendered graph will not start at zero unless specified
+                  options={{
+                    scales: {
+                      yAxes: [{
+                        ticks: {
+                          beginAtZero: true
+                        }
+                      }]
+                    }
+                  }}
+                />
               </div>
             </div>
           </div>
