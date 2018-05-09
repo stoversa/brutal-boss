e9b07b968bc6ffb3df2a499b08af1db92ed811c7
Tony Gottschalk <tony.gottschalk2017@gmail.com>
2018-05-09 09:27:42 -0400

    Re-commit with Yarn dependencies
    Heroku Deploy ... committing with Yarn.

--- server/db/index.js
+++ server/db/index.js
@@ -2,27 +2,13 @@
 * - this is where we set up our connection to the mongo database
 */
 const mongoose = require('mongoose')
-mongoose.Promise = global.Promise
-let MONGO_URL
-const MONGO_LOCAL_URL = 'mongodb://localhost/mern-passport'
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mern-passport";
 
-if (process.env.MONGODB_URI) {
-	mongoose.connect(process.env.MONGODB_URI)
-	MONGO_URL = process.env.MONGODB_URI
-} else {
-	mongoose.connect(MONGO_LOCAL_URL) // local mongo url
-	MONGO_URL = MONGO_LOCAL_URL
-}
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
-// should mongoose.connection be put in the call back of mongoose.connect???
-const db = mongoose.connection
-db.on('error', err => {
-	console.log(`There was an error connecting to the database: ${err}`)
-})
-db.once('open', () => {
-	console.log(
-		`You have successfully connected to your mongo database: ${MONGO_URL}`
-	)
-})
+const db = mongoose.connection;
 
-module.exports = db
+module.exports = db;
